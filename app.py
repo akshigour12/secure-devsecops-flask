a77154c60e92f0352ac2cf8bc867f04d4eff0df7
Akshita Gour <64450939+akshigour12@users.noreply.github.com>
2026-09-03 10:16:17 +0530

    Use environment variables for username and password
    Replaced hardcoded username and password with environment variables.

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
+import os
 from flask import Flask, render_template, request, redirect, url_for, jsonify
 
 app = Flask(__name__)
 
-USERNAME = "admin"
-PASSWORD = "admin123"
+USERNAME = os.environ["APP_USERNAME"]
+PASSWORD = os.environ["APP_PASSWORD"]
 
 
 @app.route("/")
